400b8ce675792eb4bb6004e75420eeef661d008f
Your Name <you@example.com>
2021-02-02 17:32:44 +0000

    2nd commit

--- CSY2043-wk2/myfirstreact/src/index.js
+++ CSY2043-wk2/myfirstreact/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*this is how we build a component
+class Student extends React.Component{
+	render(){
+		return <h2>I am a student</h2>;
+	}
+}
+//this is how we build a function component
+function Student(){
+	return <h1> I am a students of type function</h1>;
+}*/
+//this is how we build a class component with a constructor
+class Student extends React.Component{
+	constructor(){
+		super();
+		this.state = {webDesign: "Dislike"}
+	}
+	render(){
+		return <h2>I am a student who {this.state.webDesign} web design</h2>;
+	}
+}
+ReactDOM.render(<Student />, document.getElementById('root'));
